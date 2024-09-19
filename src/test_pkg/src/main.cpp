c55edbb1dbdbbb4d9adf6d0e996d30096736e83c
zackary jing <zackary.jing@gmail.com>
2024-09-19 17:22:09 +0800

    wip: 2024-09-19 17:22:09

--- src/test_pkg/src/main.cpp
+++ src/test_pkg/src/main.cpp
@@ -1,5 +1,4 @@
 #include <iostream>
-#include <bits/stdc++.h>
 #include <ros/ros.h>
 #include <serial/serial.h>
 
@@ -9,6 +8,7 @@ void USART_SEND(serial::Serial &ser_loc, int angle, int speed);
 
 int main()
 {
+    // 设置串口参数
     ser.setBaudrate(115200);
     ser.setPort("/dev/ttyUSB0");
     serial::Timeout to = serial::Timeout::simpleTimeout(3000);
@@ -17,32 +17,45 @@ int main()
 
     if (ser.isOpen())
     {
-        int index = 1000000;
-        for (int i = 0; i < index; i++)
+        // 设置主循环的频率，比如500Hz
+        ros::Rate looprate(500);
+
+        // 主循环，持续执行，直到ros::ok()返回false
+        while (ros::ok())
         {
             int angle = 0, speed = 0;
+
+            // 动态获取参数
             ros::param::get("angle", angle);
             ros::param::get("speed", speed);
+
+            // 发送串口信息
             USART_SEND(ser, angle, speed);
+
+            // 处理ROS回调并控制循环频率
+            ros::spinOnce();
+            looprate.sleep();
         }
     }
-    ros::Rate looprate(500);
-    while (ros::ok())
+    else
     {
-        ros::spinOnce;
-        looprate.sleep();
+        cout << "Failed to open serial port!" << endl;
     }
+
     return 0;
 }
 
 void USART_SEND(serial::Serial &ser_loc, int angle, int speed)
 {
     cout << "[DEBUG] USART_SEND called___ angle: " << angle << " speed: " << speed << endl;
-    // auto *tx_buf = new uint8_t[9];
-    // tx_buf = (uint8_t *)"R200W2\r\n\0";
-    // const char *tx_buf = "R200W2\r\n\0";
+
+    // 格式化字符串
     char tx_buf[9];
     sprintf(tx_buf,"R%03dW%d",angle,speed);
-    cout << "[DEBUG] tx_buf content" << tx_buf << endl;
+
+    // 输出调试信息
+    cout << "[DEBUG] tx_buf content: " << tx_buf << endl;
+
+    // 发送串口数据
     ser_loc.write(reinterpret_cast<const uint8_t *>(tx_buf), 8);
 }
