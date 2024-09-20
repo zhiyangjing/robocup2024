4243fc75189abfc8278b5d987d64d321d4497f9b
zackary jing <zackary.jing@gmail.com>
2024-09-20 14:58:37 +0800

    new sc script test

--- sc.sh
+++ sc.sh
@@ -3,15 +3,29 @@
 # 获取当前主机名
 HOSTNAME=$(hostname)
 
-# 获取可选的 commit 消息
-COMMIT_MESSAGE=${1}
-if [ -z "$COMMIT_MESSAGE" ]; then
-    # 如果没有提供 commit 消息，使用默认消息
-    COMMIT_MESSAGE="wip: $(date +'%Y-%m-%d %H:%M:%S')"
-fi
+# 默认的 commit 消息
+COMMIT_MESSAGE="wip: $(date +'%Y-%m-%d %H:%M:%S')"
+TAG=""
+
+# 解析参数
+while getopts "m:t:" opt; do
+    case ${opt} in
+        m )
+            COMMIT_MESSAGE=$OPTARG
+            ;;
+        t )
+            TAG=$OPTARG
+            ;;
+        \? )
+            echo "Usage: cmd [-m commit_message] [-t tag]"
+            exit 1
+            ;;
+    esac
+done
 
 # 打印当前操作的信息
 echo "Commit message: $COMMIT_MESSAGE"
+echo "Tag: $TAG"
 echo "Current host: $HOSTNAME"
 
 if [[ "$HOSTNAME" == *"Jing"* ]]; then
@@ -30,8 +44,19 @@ if [[ "$HOSTNAME" == *"Jing"* ]]; then
     # 提交更改
     git commit -m "$COMMIT_MESSAGE"
     
+    # 如果指定了 tag，则添加 tag
+    if [ -n "$TAG" ]; then
+        git tag "$TAG"
+        echo "Tag '$TAG' created."
+    fi
+    
     # 推送到远程仓库
     git push origin
+    # 推送 tag
+    if [ -n "$TAG" ]; then
+        git push origin "$TAG"
+    fi
+
 elif [[ "$HOSTNAME" == *"nano"* ]]; then
     # 如果主机名包含 'linux'，则在 Linux 主板中运行的操作
     echo "Running in Linux. Performing Git operations..."
