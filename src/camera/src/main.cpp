d0cb89331c3e4f30246366827a465661d2ba9981
zackary jing <zackary.jing@gmail.com>
2024-09-20 14:52:52 +0800

    wip: 2024-09-20 14:52:52

---
+++ src/camera/src/main.cpp
@@ -0,0 +1,33 @@
+#include <spdlog/spdlog.h>
+#include <opencv2/opencv.hpp>
+#include <ros/ros.h>
+
+int main(int argc,char** argv) {
+    ros::init(argc,argv,"camera_node");
+
+    ros::NodeHandle nh;
+
+    cv::VideoCapture cap(0);
+    if (!cap.isOpened()) {
+        ROS_ERROR("Failed to open the camera");
+        return -1;
+    }
+
+    cv::Mat frame;
+    while (ros::ok()) {
+        cap >> frame;
+        if (frame.empty()) {
+            ROS_WARN("Empty frame received");
+            break;
+        }
+        cv::imshow("Camera Feed" , frame);
+        if (cv::waitKey(30) >= 0) break;
+        ros::spinOnce();
+    }
+    cap.release();
+    cv::destroyAllWindows();
+    return 0;
+}
+
+
+
