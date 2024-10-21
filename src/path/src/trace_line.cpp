b8fc459742be5a551481c76ed17f5fc00caa0e08
zackary jing <zackary.jing@gmail.com>
2024-10-21 19:13:02 +0800

    wip: 蓝线调整策略

--- src/path/src/trace_line.cpp
+++ src/path/src/trace_line.cpp
@@ -358,15 +358,18 @@ void TraceLine::checkBlueLine() {
 
     sort(blueLines.begin(), blueLines.end(), [](const auto &a, const auto &b) { return get<1>(a) > get<1>(b); });
     auto longestLine = blueLines[0];
-    if (get<3>(longestLine)[1] > 435 and (get<1>(longestLine) > min_blue_length and blueLines.size() > 3)
-        or (get<1>(longestLine) > 130 and blueLines.size() > 6)) {
-        blue_line_found = true;
-        // ROS_INFO(TAG "%s", string(20, '-').c_str());
-        ROS_INFO(TAG COLOR_BLUE "Road Blue Line detected!" COLOR_RESET);
-        // ROS_INFO(TAG "%s", string(20, '-').c_str());
-        ROS_INFO(TAG "slope : %f length: %f", get<2>(longestLine), get<1>(longestLine));
-        ROS_INFO(TAG "center_x:  %d center_y: %d", get<3>(longestLine)[2], get<3>(longestLine)[1]);
-        // ROS_INFO(TAG "%s", string(20, '-').c_str());
+    if (get<3>(longestLine)[1] > 435) {
+        if ((get<1>(longestLine) > min_blue_length and blueLines.size() > 3)
+            or (get<1>(longestLine) > 130 and blueLines.size() > 6)) {
+            blue_line_found = true;
+            // ROS_INFO(TAG "%s", string(20, '-').c_str());
+            ROS_INFO(TAG COLOR_BLUE "Road Blue Line detected!" COLOR_RESET);
+            // ROS_INFO(TAG "%s", string(20, '-').c_str());
+            ROS_INFO(TAG "slope : %f length: %f", get<2>(longestLine), get<1>(longestLine));
+            ROS_INFO(TAG "center_x:  %d center_y: %d", get<3>(longestLine)[2], get<3>(longestLine)[1]);
+            ROS_INFO(TAG "blueLines size:  %d ", blueLines.size());
+            // ROS_INFO(TAG "%s", string(20, '-').c_str());
+        }
     }
     // 长度大于特定最小值，并且处于屏幕下方
 }
