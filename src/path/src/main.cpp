169f01284c6c1f77f99db3f30df33e60157f304b
zackary jing <zackary.jing@gmail.com>
2024-10-29 00:42:17 +0800

    wip: 2024-10-29 00:42:17

--- src/path/src/main.cpp
+++ src/path/src/main.cpp
@@ -263,13 +263,21 @@ public:
 
         // 为了对其只好这么写了
         // 应该要重新修改。改为640，480
-        reverse_park_params = ParkInitParams(13, 3);
+        reverse_park_params = ParkInitParams(16, 3);
 
-        reverse_park_params.ref_points.row(0) << 609, 627, 612, 889, 876, 370, 370, 588, 737, 743, 559, 750, 720;
-        reverse_park_params.ref_points.row(1) << 363, 370, 360, 424, 444, 311, 303, 249, 470, 489, 270, 366, 333;
-        reverse_park_params.ref_points.row(2) << 124, 136, 113, 363, 369, -136, -128, 37, 200, 253, 30, 115, 45;
-        reverse_park_params.ref_value << 0, 0, 0, -200, -200, 200, 200, 100, -100, -150, 150, -200, -200;
-        reverse_park_params.weights << 1, 2, 1;
+        // reverse_park_params.ref_points.row(0) << 609, 627, 612, 889, 876, 370, 370, 588, 737, 743, 559, 750, 720;
+        // reverse_park_params.ref_points.row(1) << 363, 370, 360, 424, 444, 311, 303, 249, 470, 489, 270, 366, 333;
+        // reverse_park_params.ref_points.row(2) << 124, 136, 113, 363, 369, -136, -128, 37, 200, 253, 30, 115, 45;
+        // reverse_park_params.ref_value << 0, 0, 0, -200, -200, 200, 200, 100, -100, -150, 150, -200, -200;
+        // reverse_park_params.weights << 1, 2, 1;
+
+        reverse_park_params.ref_points.row(0) << 650, 650, 685, 685, 600, 560, 550, 760, 778, 760, 600, 670, 750, 710,
+            650, 670;
+        reverse_park_params.ref_points.row(1) << 331, 334, 328, 328, 301, 288, 206, 359, 360, 355, 233, 275, 325, 400,
+            377, 401;
+        reverse_park_params.ref_points.row(2) << 42, 48, 31, 26, -43, -70, -54, 95, 135, 154, -50, 33, 150, 40, 44, 30;
+        reverse_park_params.ref_value << 0, 0, 0, 0, 200, 150, 100, -200, -150, -100, 100, 60, 30, -100, -60, -30;
+        reverse_park_params.weights << 1, 1, 1;
 
         //     reverse_park_params.ref_points.row(0) << 609, 627, 612, 889, 876, 370, 370, 588, 737, 743, 559;
         //     reverse_park_params.ref_points.row(1) << 363, 370, 360, 424, 444, 311, 303, 249, 470, 489, 270;
