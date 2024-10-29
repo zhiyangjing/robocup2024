e7b2b394ea8a9fcb7885a9da7e21a437615f9946
zackary jing <zackary.jing@gmail.com>
2024-10-30 06:50:12 +0800

    wip: 2024-10-30 06:50:12

--- src/path/src/forward_park.cpp
+++ src/path/src/forward_park.cpp
@@ -14,12 +14,13 @@ int main(int argc, char **argv) {
 
     ParkInitParams forward_park_params = ParkInitParams(16, 3);
 
-    forward_park_params.ref_points.row(0) << 650, 650, 685, 685, 600, 560, 550, 760, 778, 760, 600, 670, 750, 710, 650,
-        670;
-    forward_park_params.ref_points.row(1) << 331, 334, 328, 328, 301, 288, 206, 359, 360, 355, 233, 275, 325, 400, 377,
-        401;
-    forward_park_params.ref_points.row(2) << 42, 48, 31, 26, -43, -70, -54, 95, 135, 154, -50, 33, 150, 40, 44, 30;
-    forward_park_params.ref_value << 0, 0, 0, 0, -200, -150, -100, 200, 150, 100, -100, -60, -30, 100, 60, 30;
+    forward_park_params.ref_points.row(0) << 640, 645, 644, 660, 790, 761, 798, 598, 580, 570, 595, 570, 552, 740, 730,
+        662;
+    forward_park_params.ref_points.row(1) << 410, 418, 410, 401, 446, 429, 428, 379, 378, 379, 278, 264, 270, 554, 493,
+        460;
+    forward_park_params.ref_points.row(2) << 120, 130, 131, 139, 226, 180, 226, 50, 35, 60, 10, 0, 20, 230, 185, 130;
+    forward_park_params.ref_value << 0, 0, 0, 0, 200, 150, 100, -200, -150, -100, -200, -120, -60, 200, 120, 60;
+
     forward_park_params.weights << 1, 1, 1;
     forward_park_params.first_stage_param = 2;
     forward_park_params.camera = "front";
