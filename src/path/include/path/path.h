8f04d9634127042ae5018bb3fd6c51497a1cc5cf
zackary jing <zackary.jing@gmail.com>
2024-11-01 04:38:02 +0800

    wip: 2024-11-01 04:38:02

--- src/path/include/path/path.h
+++ src/path/include/path/path.h
@@ -207,7 +207,7 @@ public:
     void imageCallback(const sensor_msgs::ImageConstPtr &msg);
     void laserCallback(const sensor_msgs::LaserScan::ConstPtr &msg);
     float findFrontDistance(vector<float> ranges);
-    void visualizeLines(const vector<cv::Vec4i> &lines, int level = 0,int type = 0) ;
+    void visualizeLines(const vector<cv::Vec4i> &lines, int level, int type);
     void visualizeLidar(vector<float> distances);
     void checkBlueLine();
     void run();
